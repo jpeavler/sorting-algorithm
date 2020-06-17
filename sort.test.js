a776504d09f2e3180b1a962ecb467055e85eb36f
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-17 09:34:35 -0600

    Adjusted test cases to handle differences in array memory locations

--- sort.test.js
+++ sort.test.js
@@ -3,11 +3,11 @@ const longList = [1,3,5,7,9,2,4,6,8,10,11,99,97,95,42,66,54,101];
 const longListSorted = [1,2,3,4,5,6,7,8,9,10,11,42,54,66,95,97,99,101];
 
 test('Handles empty list', () => {
-  expect(sort([], 0, 0).length).toBe(0);
+  expect(sort([], 0, 0).length).toEqual(0);
 });
 
 test('Sorts short list', () => {
-    expect(sort([5,3,1], 0, 2)).toBe([1,3,5]);
+    expect(sort([5,3,1], 0, 2)).toEqual([1,3,5]);
 });
 
 test('Sorts list where values are identical', () => {
